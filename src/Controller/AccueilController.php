b74ca88287bd55fcdcdb148b3b8850ffc3108dc3
Ibrahim-elyazidi <ibrahim.el-yazidi@lyceedecachan-sti.fr>
2023-01-25 17:03:19 +0100

    modif accueil

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -3,17 +3,16 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AccueilController extends AbstractController
 {
     #[Route('/accueil', name: 'app_accueil')]
-    public function index(): JsonResponse
+    public function index(): Response
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/AccueilController.php',
-        ]);
+        return $this->render('accueil/index.html.twig', [
+            'controller_name' => 'AccueilController',
+        ])
     }
 }
